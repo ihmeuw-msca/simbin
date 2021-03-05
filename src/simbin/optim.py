7c22ffed132bf9f18c34289d972d490557c3d5c2
zhengp0 <zhengp@uw.edu>
2021-03-05 14:27:42 -0800

    detect infeasible prob mat

---
+++ src/simbin/optim.py
@@ -0,0 +1,70 @@
+"""
+Optimization to adjust the constraints
+"""
+from typing import Dict
+import numpy as np
+from numpy import ndarray
+from scipy.optimize import LinearConstraint, minimize
+
+
+def adjust_prob_mat(prob_mat: ndarray,
+                    weights: ndarray = None,
+                    rtol: float = 1e-4,
+                    atol: float = 1e-4,
+                    options: Dict = None) -> ndarray:
+    ndim = prob_mat.shape[0]
+    size = 2**ndim
+
+    weights = np.ones((ndim, ndim)) if weights is None else weights
+    A = np.empty(shape=(0, size))
+    b = np.empty(0)
+    w = np.empty(0)
+
+    ind = [[0, 1]]*ndim
+    for i in range(ndim):
+        for j in range(i, ndim):
+            ind[i] = [1]
+            ind[j] = [1]
+            row = np.zeros((2,)*ndim)
+            row[tuple(np.meshgrid(*ind))] = 1.0
+            A = np.vstack([A, row.ravel()])
+            b = np.hstack([b, prob_mat[i, j]])
+            w = np.hstack([w, weights[i, j]])
+            ind[i] = [0, 1]
+            ind[j] = [0, 1]
+
+    # create optimization problem
+    def objective(x: ndarray) -> float:
+        return 0.5*w.dot((A.dot(x) - b)**2)
+
+    def gradient(x: ndarray) -> ndarray:
+        return (A.T*w).dot(A.dot(x) - b)
+
+    def hessian(x: ndarray) -> ndarray:
+        return (A.T*w).dot(A)
+
+    x0 = np.ones(size)/size
+    bounds = np.hstack([np.zeros((size, 1)), np.ones((size, 1))])
+    constraints = [LinearConstraint(np.ones((1, size)), np.ones(1), np.ones(1))]
+
+    result = minimize(
+        objective, x0,
+        method="trust-constr",
+        jac=gradient,
+        hess=hessian,
+        constraints=constraints,
+        bounds=bounds,
+        options=options
+    )
+
+    x = result.x
+    adjusted_b = A.dot(x)
+    adjusted_prob_mat = prob_mat.copy()
+    if not np.allclose(adjusted_b, b, rtol=rtol, atol=atol):
+        print("Adjust probablility matrix.")
+        k = 0
+        for i in range(ndim):
+            for j in range(i, ndim):
+                adjusted_prob_mat[i, j] = adjusted_b[k]
+                k += 1
+    return adjusted_prob_mat
--- src/simbin/random.py
+++ src/simbin/random.py
@@ -4,6 +4,7 @@ Random sampling function
 import numpy as np
 from numpy import ndarray
 from simbin.geometry import get_prob_vertices
+from simbin.optim import adjust_prob_mat
 
 
 def sample_simplex(ndim: int, size: int = 1) -> ndarray:
@@ -23,5 +24,4 @@ def sample_simplex(ndim: int, size: int = 1) -> ndarray:
 def sample_bin_prob(prob_mat: ndarray, size: int = 1) -> ndarray:
     vertices = get_prob_vertices(prob_mat)
     weights = sample_simplex(vertices.shape[0], size=size)
-
     return weights.dot(vertices)
